b27479cb795079e895919ba05565a98fd8ded4b4
sakura <whitecoralflame@gmail.com>
2024-11-03 20:40:06 -0500

    11/3 RobotGo, motor test

---
+++ timed_basic_motor/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,11 @@
+//All constants for the robot
+
+package frc.robot;
+
+final public class Constants {
+    //
+    //Hardware CAN IDs
+    //
+    public static final int TEST_MOTOR_CAN_ID = 30;
+    
+}
