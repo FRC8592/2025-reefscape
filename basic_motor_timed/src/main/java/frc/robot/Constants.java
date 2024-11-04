dc5438c695872a78bbb9ebade45e4ab3b76ef707
Zohaib Ghumman <zohaibghumman2010@gmail.com>
2024-11-03 20:39:09 -0500

    Tested our code motor can move

---
+++ basic_motor_timed/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+// All of the Constants for the robot
+
+package frc.robot;
+
+final public class Constants {
+    //
+    // Hardware CAN Ids
+    //
+    public static final int TEST_MOTOR_CAN_ID = 30;
+}
