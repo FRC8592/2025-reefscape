dc06b8e39454136db57da299fb6c23af00ae5114
Jack Runski <frc8592-jackr@novalabsrobotics.org>
2024-11-03 20:40:46 -0500

    Made a branch and made a simple motor code

---
+++ Coolest motor in the west/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,11 @@
+// All of the constants for robot
+
+package frc.robot;
+
+
+final public class Constants {
+    //
+    // Hardware CAN ids
+//
+ public static final int TEST_MOTOR_CAN_ID = 30;
+}
