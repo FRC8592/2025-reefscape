36872e97d29374f92f1dd106c6c69c3f5fee7193
Joe Thompson <jgthomps@gmail.com>
2024-11-10 17:35:01 -0500

    Code with separate motor class.

---
+++ timed_basic_motor/src/main/java/frc/robot/DriveTrain.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+import com.ctre.phoenix6.hardware.*;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.*;
+
+public class DriveTrain {
+    private TalonFX testMotor;
+    VoltageOut testMotorVoltage = new VoltageOut(0); // Datatype needed to control voltage
+
+    public DriveTrain () {
+        TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+
+        // Create the controller object for our test motor
+        testMotor = new TalonFX(Constants.TEST_MOTOR_CAN_ID);
+
+        // Set factory defaults for all motor configuration options
+        testMotor.getConfigurator().apply(talonFXConfigs);
+
+        // Set the motor to zero power for safety
+        testMotor.setControl(testMotorVoltage.withOutput(0));
+    }
+
+    /**
+     * Set the motor power in terms of percent
+     * @param outputPercent Specify motor power in a range of -1 to +1
+     */
+    public void setPercentMotorOutput(double outputPercent) {
+        double motorVoltage = testMotor.getSupplyVoltage().getValueAsDouble();
+    
+        testMotor.setControl(testMotorVoltage.withOutput(motorVoltage * outputPercent));
+    }
+
+}
--- timed_basic_motor/src/main/java/frc/robot/Robot.java
+++ timed_basic_motor/src/main/java/frc/robot/Robot.java
@@ -7,8 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import com.ctre.phoenix6.hardware.*;
-import com.ctre.phoenix6.controls.*;
 import edu.wpi.first.wpilibj.XboxController;
 // import frc.robot.Constants;
 
@@ -24,8 +22,7 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  private TalonFX testMotor;
-  //
+  private DriveTrain drive;
   private XboxController gamepad = new XboxController(0);
 
   /**
@@ -34,15 +31,11 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    drive = new DriveTrain();
+
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-
-    // Create the controller object for our test motor
-    testMotor = new TalonFX(Constants.TEST_MOTOR_CAN_ID);
-
-    // Create the controller object for our gamepad controller
-
   }
 
   /**
@@ -94,15 +87,8 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    VoltageOut testMotorVoltage = new VoltageOut(0);
-    double commandedVelocity = gamepad.getRightY();
-    double motorVoltage = testMotor.getSupplyVoltage().getValueAsDouble();
-    
-    SmartDashboard.putNumber("Joystick Value", commandedVelocity);
-    SmartDashboard.putNumber("Motor V", motorVoltage);
-
-   testMotor.setControl(testMotorVoltage.withOutput(13*commandedVelocity));
-
+    double percentPower = gamepad.getRightY();
+    drive.setPercentMotorOutput(percentPower);
   }
 
   /** This function is called once when the robot is disabled. */
