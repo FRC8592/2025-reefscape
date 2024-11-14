6b9f14c4af693696bdabfaa6eab9ffb840267899
ayanp26 <ayanprasad26@gmail.com>
2024-11-13 19:49:22 -0500

    Worked on test motor with command base

---
+++ command_motor/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import java.beans.DesignMode;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.*;
+import com.ctre.phoenix6.hardware.*;
+
+public class DriveTrain extends SubsystemBase {
+  TalonFX testMotor;
+  VoltageOut desiredMotorVoltage = new VoltageOut(0);
+
+  public DriveTrain() {
+    //Setting factory defaults for the motor
+    TalonFXConfiguration factoryConfigs = new TalonFXConfiguration();
+
+    //Initializing the test motor
+    testMotor = new TalonFX(Constants.MAIN_MOTOR_CAN_ID);
+    testMotor.getConfigurator().apply(factoryConfigs);
+  }
+
+  /**
+   * Get the actual voltage available at the motor input
+   * @return
+   */
+  public double getMaxMotorVoltage() {
+    return testMotor.getSupplyVoltage().getValueAsDouble();
+  }
+
+  /**
+   * Set the motor percent output
+   * @param percentPower
+   */
+  public void setMotorPercentOutput(double percentPower) {
+    testMotor.setControl(desiredMotorVoltage.withOutput(percentPower * getMaxMotorVoltage()));
+  }
+
+}
