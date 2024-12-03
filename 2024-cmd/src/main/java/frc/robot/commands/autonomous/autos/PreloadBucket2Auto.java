57b5a1185318b6bc1e03ccfb0cf1b54b699b0f7a
Martin <mc3441272@gmail.com>
2024-12-02 19:54:32 -0500

    merged autonomous code in

--- 2024-cmd/src/main/java/frc/robot/commands/autonomous/autos/PreloadBucket2Auto.java
+++ 2024-cmd/src/main/java/frc/robot/commands/autonomous/autos/PreloadBucket2Auto.java
@@ -3,16 +3,17 @@ package frc.robot.commands.autonomous.autos;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Suppliers;
 import frc.robot.commands.autonomous.AutoCommand;
+import frc.robot.commands.largecommands.*;
 
 public class PreloadBucket2Auto extends AutoCommand {
     
     public PreloadBucket2Auto() {
         super(
             
-            swerve.commands.followPathCommand(getChoreoTrajectory("PreloadBucket2Path"), Suppliers.robotRunningOnRed)
+            new FollowPathCommand(getChoreoTrajectory("PreloadBucket2Path"), Suppliers.robotRunningOnRed)
             .andThen(Commands.waitSeconds(1))
             .andThen(
-                swerve.commands.followPathCommand(getChoreoTrajectory("PreloadBucket2Path2"), Suppliers.robotRunningOnRed)
+                new FollowPathCommand(getChoreoTrajectory("PreloadBucket2Path2"), Suppliers.robotRunningOnRed)
             )
 
         );
