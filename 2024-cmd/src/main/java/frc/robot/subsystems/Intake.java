555f35ea7f6856e1dfc9a17b2141a2049fb19cbb
Zaid Rasti <zaid.rski@gmail.com>
2025-01-11 14:50:46 -0500

    added commanded vs recorded velocity logging

--- 2024-cmd/src/main/java/frc/robot/subsystems/Intake.java
+++ 2024-cmd/src/main/java/frc/robot/subsystems/Intake.java
@@ -68,6 +68,11 @@ public class Intake extends SubsystemBase {
         Logger.recordOutput(INTAKE.LOG_PATH+"OuterMotorCommandedVelocity", outerMotorCommandedVelocity);
         Logger.recordOutput(INTAKE.LOG_PATH+"InnerMotorCommandedVelocity", innerMotorCommandedVelocity);
         Logger.recordOutput(INTAKE.LOG_PATH+"WristMotorCommandedVelocity", wristMotorCommandedVelocity);
+
+        Logger.recordOutput(INTAKE.LOG_PATH+"Outer Motor Commanded vs Recorded Velocity", outerMotorCommandedVelocity-getOuterMotorVelocity());
+        Logger.recordOutput(INTAKE.LOG_PATH+"Inner Motor Commanded vs Recorded Velocity", innerMotorCommandedVelocity-getInnerMotorVelocity());
+        Logger.recordOutput(INTAKE.LOG_PATH+"Wrist Motor Commanded vs Recorded Velocity", wristMotorCommandedVelocity-getWristMotorVelocity());
+
     }
 
     public double getOuterMotorVelocity()
