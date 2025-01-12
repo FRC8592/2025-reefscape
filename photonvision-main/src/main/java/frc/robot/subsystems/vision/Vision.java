e4ee8e49ce47f7bf1f2a7c12b56c2daf14322d92
yuii01 <whitecoralflame@gmail.com>
2025-01-11 19:32:24 -0500

    changed cam for robot

--- photonvision-main/src/main/java/frc/robot/subsystems/vision/Vision.java
+++ photonvision-main/src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class Vision implements Subsystem{
-    PhotonCamera camera = new PhotonCamera("Arducam_OV9782_D");
+    PhotonCamera camera = new PhotonCamera("Arducam_OV9782_C");
 
     public Vision(){
     }
