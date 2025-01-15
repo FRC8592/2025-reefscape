690e14b6498363fec0e1ef49b51c779e0333f979
Nassassiin <nathanxavier165@gmail.com>
2025-01-15 18:52:02 -0500

    made skeleton code branch and merged elevator code into it

--- 2024-cmd/src/main/java/frc/robot/commands/autonomous/AutoManager.java
+++ 2024-cmd/src/main/java/frc/robot/commands/autonomous/AutoManager.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Robot;
 import frc.robot.Suppliers;
-import frc.robot.commands.autonomous.autos.*;
 import frc.robot.commands.proxies.*;
 import frc.robot.subsystems.swerve.Swerve;
 import frc.robot.subsystems.*;
