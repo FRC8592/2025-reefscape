7804fe5138a4b56746ed2dfb63f974546077c5e9
yuii01 <whitecoralflame@gmail.com>
2025-01-26 16:22:39 -0500

    implemented new swerve constants

--- 2025-1-26-GeneratedSwerve/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ 2025-1-26-GeneratedSwerve/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.swerve.ctreswerve;
+package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.*;
 
@@ -22,8 +22,8 @@ import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.subsystems.swerve.ctreswerve.TunerConstants.TunerSwerveDrivetrain;
 
+import frc.robot.generated.TunerConstants.TunerSwerveDrivetrain;
 
 /**
  * Class that extends the Phoenix 6 SwerveDrivetrain class and implements
