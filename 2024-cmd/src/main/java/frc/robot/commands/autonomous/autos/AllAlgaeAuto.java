2ec4443a07b3f78c91dc4bdb638fbd28a8dad85d
baaisha <naaisha.mahajan@gmail.com>
2025-03-31 14:42:01 -0400

    Updated All Algae Auto so it only takes one algae.

--- 2024-cmd/src/main/java/frc/robot/commands/autonomous/autos/AllAlgaeAuto.java
+++ 2024-cmd/src/main/java/frc/robot/commands/autonomous/autos/AllAlgaeAuto.java
@@ -29,12 +29,27 @@ public class AllAlgaeAuto extends AutoCommand{
                 0.5,
                 false,
                 false
+            ),
+
+            new FollowPathCommand(
+                getChoreoTrajectory("DAlgaeBackUp"),
+                Suppliers.isRedAlliance,
+                "",
+                0.5,
+                false,
+                false
             )
             .alongWith(new WaitCommand(0.25).andThen(
-                scoring.goToSpecifiedPositionCommand(ElevatorPositions.getL2Algae()),
-                scoring.intakeCommand().withTimeout(2)
+                scoring.goToSpecifiedPositionCommand(ElevatorPositions.getL2Algae())
             )),
-
+            new FollowPathCommand(
+                getChoreoTrajectory("MiddleToDAlgae"),
+                Suppliers.isRedAlliance,
+                "",
+                0.5,
+                false,
+                false
+            ).andThen(scoring.intakeCommand().withTimeout(2)),
             new FollowPathCommand(
                 getChoreoTrajectory("DAlgaeToBlueNet"),
                 Suppliers.isRedAlliance,
@@ -44,55 +59,55 @@ public class AllAlgaeAuto extends AutoCommand{
                 false
             )
             .alongWith(new WaitCommand(0.25).andThen(scoring.goToSpecifiedPositionCommand(ElevatorPositions.getNet())))
-            .andThen(new WaitCommand(0.75), scoring.outtakeCoralCommand().withTimeout(0.25)),
-
-            new FollowPathCommand(
-                getChoreoTrajectory("BlueNetToEAlgae"),
-                Suppliers.isRedAlliance,
-                "",
-                0.5,
-                false,
-                false
-            )
-            .alongWith(new WaitCommand(0.25).andThen(
-                scoring.goToSpecifiedPositionCommand(ElevatorPositions.getL2Algae()),
-                scoring.intakeCommand().withTimeout(2)
-            )),
-
-            new FollowPathCommand(
-                getChoreoTrajectory("EAlgaeToBlueNet"),
-                Suppliers.isRedAlliance,
-                "",
-                0.5,
-                false,
-                false
-            )
-            .alongWith(new WaitCommand(0.25).andThen(scoring.goToSpecifiedPositionCommand(ElevatorPositions.getNet())))
-            .andThen(new WaitCommand(0.75), scoring.outtakeCoralCommand().withTimeout(0.25)),
-
-            new FollowPathCommand(
-                getChoreoTrajectory("BlueNetToCAlgae"),
-                Suppliers.isRedAlliance,
-                "",
-                0.5,
-                false,
-                false
-            )
-            .alongWith(new WaitCommand(0.25).andThen(
-                scoring.goToSpecifiedPositionCommand(ElevatorPositions.getL2Algae()),
-                scoring.intakeCommand().withTimeout(2)
-            )),
-
-            new FollowPathCommand(
-                getChoreoTrajectory("CAlgaeToBlueNet"),
-                Suppliers.isRedAlliance,
-                "",
-                0.5,
-                false,
-                false
-            )
-            .alongWith(new WaitCommand(0.25).andThen(scoring.goToSpecifiedPositionCommand(ElevatorPositions.getNet())))
             .andThen(new WaitCommand(0.75), scoring.outtakeCoralCommand().withTimeout(0.25))
+
+            // new FollowPathCommand(
+            //     getChoreoTrajectory("BlueNetToEAlgae"),
+            //     Suppliers.isRedAlliance,
+            //     "",
+            //     0.5,
+            //     false,
+            //     false
+            // )
+            // .alongWith(new WaitCommand(0.25).andThen(
+            //     scoring.goToSpecifiedPositionCommand(ElevatorPositions.getL2Algae()),
+            //     scoring.intakeCommand().withTimeout(2)
+            // )),
+
+            // new FollowPathCommand(
+            //     getChoreoTrajectory("EAlgaeToBlueNet"),
+            //     Suppliers.isRedAlliance,
+            //     "",
+            //     0.5,
+            //     false,
+            //     false
+            // )
+            // .alongWith(new WaitCommand(0.25).andThen(scoring.goToSpecifiedPositionCommand(ElevatorPositions.getNet())))
+            // .andThen(new WaitCommand(0.75), scoring.outtakeCoralCommand().withTimeout(0.25)),
+
+            // new FollowPathCommand(
+            //     getChoreoTrajectory("BlueNetToCAlgae"),
+            //     Suppliers.isRedAlliance,
+            //     "",
+            //     0.5,
+            //     false,
+            //     false
+            // )
+            // .alongWith(new WaitCommand(0.25).andThen(
+            //     scoring.goToSpecifiedPositionCommand(ElevatorPositions.getL2Algae()),
+            //     scoring.intakeCommand().withTimeout(2)
+            // )),
+
+            // new FollowPathCommand(
+            //     getChoreoTrajectory("CAlgaeToBlueNet"),
+            //     Suppliers.isRedAlliance,
+            //     "",
+            //     0.5,
+            //     false,
+            //     false
+            // )
+            // .alongWith(new WaitCommand(0.25).andThen(scoring.goToSpecifiedPositionCommand(ElevatorPositions.getNet())))
+            // .andThen(new WaitCommand(0.75), scoring.outtakeCoralCommand().withTimeout(0.25))
         );
 
         setStartStateFromChoreoTrajectory("LeftToCMid");
