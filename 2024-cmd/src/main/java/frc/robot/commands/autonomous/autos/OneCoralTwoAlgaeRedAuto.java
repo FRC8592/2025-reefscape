9638f9e667ccf2d99fd06ae512f920ab75691b91
FRC8592-Anonymous <github.501ov@slmails.com>
2025-04-17 15:29:50 -0400

    added one coral two algae

--- 2024-cmd/src/main/java/frc/robot/commands/autonomous/autos/OneCoralTwoAlgaeRedAuto.java
+++ 2024-cmd/src/main/java/frc/robot/commands/autonomous/autos/OneCoralTwoAlgaeRedAuto.java
@@ -1,6 +1,11 @@
 package frc.robot.commands.autonomous.autos;
 
+import java.util.Set;
+
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Suppliers;
 import frc.robot.commands.autonomous.AutoCommand;
 import frc.robot.commands.largecommands.FollowPathCommand;
@@ -9,24 +14,62 @@ import frc.robot.subsystems.scoring.Scoring.ElevatorPositions;
 public class OneCoralTwoAlgaeRedAuto extends AutoCommand{
     public OneCoralTwoAlgaeRedAuto(DriverStation.Alliance color){
         super(
-            new FollowPathCommand(getChoreoTrajectory("MiddleToDRight"+color.name()), Suppliers.isRedAlliance, "")
+             new FollowPathCommand(getChoreoTrajectory("MiddleToDRight"+color.name()), Suppliers.isRedAlliance, "")
             .alongWith(scoring.goToPosition(ElevatorPositions.getL4()))
-            .andThen(scoring.outtakeCoralCommand().withTimeout(0.2)),
+            .andThen(Commands.waitSeconds(0.25), scoring.outtakeCoralCommand().withTimeout(0.2)),
+        
             scoring.setAlgaeMode(),
+
             new FollowPathCommand(getChoreoTrajectory("DRightToMiddle"+color.name()), Suppliers.isRedAlliance, "")
-            .andThen(scoring.goToPosition(ElevatorPositions.getL2Algae()).withTimeout(0.5)),
-            new FollowPathCommand(getChoreoTrajectory("MiddleToDMiddle"), Suppliers.isRedAlliance, ""),
+            .alongWith(new WaitCommand(0.5).andThen(scoring.goToPosition(ElevatorPositions.getL2Algae()))),
+
+            new FollowPathCommand(getChoreoTrajectory("MiddleToDMiddle"), Suppliers.isRedAlliance, "")
+            .andThen(scoring.intakeCommand().withTimeout(0.5)),
+
             new FollowPathCommand(getChoreoTrajectory("DAlgaeTo"+color.name()+"Net"), Suppliers.isRedAlliance, "")
-            .andThen(scoring.goToPosition(ElevatorPositions.getNet()))
-            .andThen(scoring.outtakeCoralCommand().withTimeout(0.5))
-            .andThen(scoring.goToPosition(ElevatorPositions.getL3Algae())),
-            new FollowPathCommand(getChoreoTrajectory(color.name()+"NetToCAlgae"), Suppliers.isRedAlliance, ""),
-            new FollowPathCommand(getChoreoTrajectory("CAlgaeTo"+color.name()+"Net"), Suppliers.isRedAlliance, "")
-            .andThen(scoring.goToPosition(ElevatorPositions.getNet()))
-            .andThen(scoring.outtakeCoralCommand().withTimeout(0.5))
-            .andThen(scoring.setCoralMode())
-            .andThen(scoring.goToPosition(ElevatorPositions.getStow())),
+            .deadlineFor(scoring.intakeCommand(), Commands.waitSeconds(0.5).andThen(scoring.goToPosition(ElevatorPositions.getNet()))),
+
+            // new WaitUntilCommand(() -> scoring.atPosition())
+            // .deadlineFor(scoring.intakeCommand()),
+
+            
+            scoring.outtakeCoralCommand().withTimeout(1),
+
+            scoring.setAlgaeMode(),
+
+            Commands.defer(
+                () -> (
+                    Suppliers.isRedAlliance.getAsBoolean()
+                    ? (new FollowPathCommand(getChoreoTrajectory("RedNetToCAlgae"), Suppliers.isRedAlliance, "").alongWith(new WaitCommand(0.5).andThen(scoring.goToPosition(ElevatorPositions.getL3Algae()))).andThen(scoring.intakeCommand().withTimeout(0.5)))
+                    : (new FollowPathCommand(getChoreoTrajectory("BlueNetToEAlgae"), Suppliers.isRedAlliance, "").alongWith(new WaitCommand(0.5).andThen(scoring.goToPosition(ElevatorPositions.getL3Algae()))).andThen(scoring.intakeCommand().withTimeout(0.5)))
+                ),
+                Set.of(swerve)
+                
+            ),
+
+            Commands.defer(
+                () -> (
+                    Suppliers.isRedAlliance.getAsBoolean()
+                    ? (new FollowPathCommand(getChoreoTrajectory("CAlgaeToRedNet"), Suppliers.isRedAlliance, "").deadlineFor(scoring.intakeCommand(), Commands.waitSeconds(0.5).andThen(scoring.goToPosition(ElevatorPositions.getNet()))))
+                    : (new FollowPathCommand(getChoreoTrajectory("EAlgaeToBlueNet"), Suppliers.isRedAlliance, "").deadlineFor(scoring.intakeCommand(), Commands.waitSeconds(0.5).andThen(scoring.goToPosition(ElevatorPositions.getNet()))))
+                ),
+            Set.of(swerve)
+            ),
+
+            new WaitUntilCommand(() -> scoring.atPosition())
+            .deadlineFor(scoring.intakeCommand()),
+
+            scoring.outtakeCoralCommand().withTimeout(1),
+
+            scoring.setCoralMode(), 
+            
             new FollowPathCommand(getChoreoTrajectory(color.name()+"NetBackUp"), Suppliers.isRedAlliance, "")
+            .alongWith(new WaitCommand(0.5).andThen(scoring.goToPosition(ElevatorPositions.getStow())))
+            
+            
+            
+            
+             
         );
 
     }
