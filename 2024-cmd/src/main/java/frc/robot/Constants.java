9dc0444256e8c93f05036a3b551280b9e9d4ba31
yuii01 <whitecoralflame@gmail.com>
2025-09-18 19:35:30 -0400

    code for driver's training w/ coach jesse. redeploy this branch whenever it's a drive night

--- 2024-cmd/src/main/java/frc/robot/Constants.java
+++ 2024-cmd/src/main/java/frc/robot/Constants.java
@@ -116,8 +116,8 @@ public final class Constants {
         public static final double DRIVE_FRICTION_VOLTAGE = 0.25;
 
         //TODO: Tone these down appropriately as per BB rules
-        public static final double TRANSLATE_POWER_FAST = 0.5; 
-        public static final double ROTATE_POWER_FAST = 0.3; 
+        public static final double TRANSLATE_POWER_FAST = 0.6; 
+        public static final double ROTATE_POWER_FAST = 0.4; 
         public static final double TRANSLATE_POWER_SLOW = 0.3; //formerly 0.5
         public static final double ROTATE_POWER_SLOW = 0.2; //formerly 0.3
 
