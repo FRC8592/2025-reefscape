9098ceb5d585040cf828c857a9129b5713eff77e
yuii01 <whitecoralflame@gmail.com>
2025-09-27 08:05:01 -0400

    updated all can encoder offset constants, white still misbehaving.

--- 2024-cmd/src/main/java/frc/robot/subsystems/swerve/riptideswerve/RiptideConstants.java
+++ 2024-cmd/src/main/java/frc/robot/subsystems/swerve/riptideswerve/RiptideConstants.java
@@ -129,7 +129,7 @@ public class RiptideConstants {
     private static final int kFrontLeftSteerMotorId = 4;
     private static final int kFrontLeftEncoderId = 10;
     // private static final Angle kFrontLeftEncoderOffset = Rotations.of(-0.232666015625);   initial value
-    private static final Angle kFrontLeftEncoderOffset = Rotations.of(-0.2548828125);
+    private static final Angle kFrontLeftEncoderOffset = Rotations.of(-0.498); //correct
     private static final boolean kFrontLeftSteerMotorInverted = true;
     private static final boolean kFrontLeftEncoderInverted = false;
 
@@ -141,7 +141,7 @@ public class RiptideConstants {
     private static final int kFrontRightSteerMotorId = 9;
     private static final int kFrontRightEncoderId = 13;
     // private static final Angle kFrontRightEncoderOffset = Rotations.of(0.307861328125);    initial value
-    private static final Angle kFrontRightEncoderOffset = Rotations.of(0.309326171875);
+    private static final Angle kFrontRightEncoderOffset = Rotations.of(-0.25); //mostly correct
     private static final boolean kFrontRightSteerMotorInverted = true;
     private static final boolean kFrontRightEncoderInverted = false;
 
@@ -153,7 +153,7 @@ public class RiptideConstants {
     private static final int kBackLeftSteerMotorId = 6;
     private static final int kBackLeftEncoderId = 11;
     //private static final Angle kBackLeftEncoderOffset = Rotations.of(0.0439453125);    initial value
-    private static final Angle kBackLeftEncoderOffset = Rotations.of(-0.04736328125);
+    private static final Angle kBackLeftEncoderOffset = Rotations.of(0.043); //misbehaving 
     
     private static final boolean kBackLeftSteerMotorInverted = true;
     private static final boolean kBackLeftEncoderInverted = false;
@@ -166,7 +166,7 @@ public class RiptideConstants {
     private static final int kBackRightSteerMotorId = 8;
     private static final int kBackRightEncoderId = 12;
     // private static final Angle kBackRightEncoderOffset = Rotations.of(0.35986328125);     initial value
-    private static final Angle kBackRightEncoderOffset = Rotations.of(0.368896484375);
+    private static final Angle kBackRightEncoderOffset = Rotations.of(0.368896484375); //correct
     private static final boolean kBackRightSteerMotorInverted = true;
     private static final boolean kBackRightEncoderInverted = false;
 
