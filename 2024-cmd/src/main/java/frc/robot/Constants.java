4ea1a334a9aaad54cb28feba80a4db0dbb4a34e1
Christopher O'Connell <coconnell84@gmail.com>
2025-10-12 20:38:36 -0400

    Redid the measurment of the camera z position for riptide

--- 2024-cmd/src/main/java/frc/robot/Constants.java
+++ 2024-cmd/src/main/java/frc/robot/Constants.java
@@ -52,7 +52,7 @@ public final class Constants {
 
         public static final Transform3d CAMERA_OFFSETS = (
             SHARED.IS_RIPTIDE
-            ? /* RIPRIDE: */ new Transform3d(new Translation3d(0.21, 0.18415, 0.24), new Rotation3d(0, Math.toRadians(-12), 0))
+            ? /* RIPRIDE: */ new Transform3d(new Translation3d(0.21, 0.18415, 0.235), new Rotation3d(0, Math.toRadians(-12), 0))
             : /* PERRY: */   new Transform3d(new Translation3d(0.17145, 0.20955, 0.2286), new Rotation3d(0, Math.toRadians(-13), Math.toRadians(-3)))
         );
 
